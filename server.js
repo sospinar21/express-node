182d6e11b7b71328abded2b011d34bf2a05c0863
sospinar21 <sospinar21@gmail.com>
2018-06-23 13:07:09 -0600

    First commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const urlLogger = (request, response, next) => {
+  console.log('Request URL:', request.url);
+  next();
+};
+
+const timeLogger = (request, response, next) => {
+  console.log('Datetime:', new Date(Date.now()).toString());
+  next();
+}
+
+app.use(urlLogger, timeLogger);
+app.use(express.static('public'));
+app.use('/sunsets', express.static('public/sunsets'));
+
+app.get('/json', urlLogger, timeLogger, (request, response) => {
+  response.status(200).json({"name": "Robbie"});
+});
+
+app.get('./sunsets', (request,response) => {
+  response.status(200).json();
+})
+
+app.listen(3000, () => {
+  console.log('Express intro running on localhost:3000');
+});
+
